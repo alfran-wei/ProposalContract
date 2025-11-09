d98d22e2d6c8de65182816dad2856442d705f0a9
alfran-wei <57835030+alfran-wei@users.noreply.github.com>
2025-11-09 19:35:43 +0000

    Update ProposalContract.sol

--- ProposalContract.sol
+++ ProposalContract.sol
@@ -2,121 +2,104 @@
 pragma solidity ^0.8.18;
 
 contract ProposalContract {
-    address owner ;
-    uint256 counter ;
+    address public owner;
+    uint256 public counter;
+
     struct MoreDescription {
         string title;
         uint date;
         string total_description;
     }
+
     struct Proposal {
-        MoreDescription description; // Description of the proposal
-        uint256 approve; // Number of approve votes
-        uint256 reject; // Number of reject votes
-        uint256 pass; // Number of pass votes
-        uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
-        bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
-        bool is_active; // This shows if others can vote to our contract
-    }
-    
-    mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
-    address[]  private voted_addresses;//recording address already use for vote
-
-
-    constructor() { // for define owner of contract
-         owner = msg.sender;
-         voted_addresses.push(msg.sender);
+        MoreDescription description;
+        uint256 approve;
+        uint256 reject;
+        uint256 pass;
+        uint256 total_vote_to_end;
+        bool current_state;
+        bool is_active;
+        address[] voters;
     }
 
-    modifier onlyOwner() { // for give access for some function just owner 
-        require(msg.sender == owner,"only owner can run this function");
-    _;
+    mapping(uint256 => Proposal) public proposal_history;
+
+    constructor() {
+        owner = msg.sender;
     }
 
-    modifier newVoter(address _address) {
-        require(!isVoted(_address), "Address has already voted");
-    _;
-    }
-    
-    //create a new proposal 
-    function create(string memory title,string calldata _description, uint256 _total_vote_to_end) external onlyOwner {
-    counter += 1;
-    proposal_history[counter] = Proposal(MoreDescription(title,block.timestamp,_description), 0, 0, 0, _total_vote_to_end, false, true);
-    }
-    
-    // owner can change address to another owner 
-    function setOwner(address new_owner) external onlyOwner {
-    owner = new_owner;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can run this function");
+        _;
     }
 
-    // function to make a vote 
-    function vote(uint8 choice) external {
-    // Function logic
-    // First part
-        Proposal storage proposal = proposal_history[counter];
-        uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
+    modifier active(uint256 id) {
+        require(proposal_history[id].is_active, "Proposal not active");
+        _;
+    }
 
-        // Second part
-        voted_addresses.push(msg.sender);
-        if (choice == 1) {
-            proposal.approve += 1;
-            proposal.current_state = calculateCurrentState();
-        } else if (choice == 2) {
-            proposal.reject += 1;
-            proposal.current_state = calculateCurrentState();
-        } else if (choice == 0) {
-            proposal.pass += 1;
-            proposal.current_state = calculateCurrentState();
-        }
+    // Create a new proposal
+    function create(
+        string memory title,
+        string calldata _description,
+        uint256 _total_vote_to_end
+    ) external onlyOwner {
+        counter += 1;
+        Proposal storage p = proposal_history[counter];
+        p.description = MoreDescription(title, block.timestamp, _description);
+        p.total_vote_to_end = _total_vote_to_end;
+        p.is_active = true;
+    }
 
-        // Third part
-        if ((proposal.total_vote_to_end - total_vote == 1) && (choice == 1 || choice == 2 || choice == 0)) {
+    // Vote on a specific proposal by ID
+    function vote(uint256 proposalId, uint8 choice) external active(proposalId) {
+        Proposal storage proposal = proposal_history[proposalId];
+        require(!hasVoted(proposalId, msg.sender), "You already voted");
+
+        proposal.voters.push(msg.sender);
+
+        if (choice == 1) proposal.approve += 1;
+        else if (choice == 2) proposal.reject += 1;
+        else if (choice == 0) proposal.pass += 1;
+
+        proposal.current_state = (proposal.approve > proposal.reject);
+
+        uint256 total_votes = proposal.approve + proposal.reject + proposal.pass;
+        if (total_votes >= proposal.total_vote_to_end) {
             proposal.is_active = false;
-            voted_addresses = [owner];
         }
-            }
-
-    modifier active() {
-    require(proposal_history[counter].is_active == true, "The proposal is not active");
-    _;
     }
 
-
-    // in my logic i don't consider pass voted
-    function calculateCurrentState() private view returns(bool) {
-    Proposal storage proposal = proposal_history[counter];
-
-    uint256 approve = proposal.approve;
-    uint256 reject = proposal.reject;
-    
-    if (approve > reject ) {
-        return true;
-    } else {
+    // Check if address already voted for this proposal
+    function hasVoted(uint256 id, address voter) public view returns (bool) {
+        address[] storage voters = proposal_history[id].voters;
+        for (uint i = 0; i < voters.length; i++) {
+            if (voters[i] == voter) return true;
+        }
         return false;
     }
+
+    // Get one proposal by ID
+    function getProposal(uint256 id) external view returns (Proposal memory) {
+        return proposal_history[id];
     }
 
-    function teminateProposal() external onlyOwner active {
-    proposal_history[counter].is_active = false;
-    }
-
-
-    function isVoted(address _address) public view returns (bool) {
-    for (uint i = 0; i < voted_addresses.length; i++) {
-        if (voted_addresses[i] == _address) {
-            return true;
+    // Get all proposals
+    function getAllProposals() external view returns (Proposal[] memory) {
+        Proposal[] memory all = new Proposal[](counter);
+        for (uint256 i = 0; i < counter; i++) {
+            all[i] = proposal_history[i + 1];
         }
-    }
-    return false;
-    }
-
-    function getCurrentProposal() external view returns(Proposal memory) {
-    return proposal_history[counter];
+        return all;
     }
 
-    function getProposal(uint256 number) external view returns(Proposal memory) {
-    return proposal_history[number];
+    // Manually close a proposal
+    function closeProposal(uint256 id) external onlyOwner {
+        proposal_history[id].is_active = false;
     }
 
-
+    // Change owner
+    function setOwner(address new_owner) external onlyOwner {
+        owner = new_owner;
+    }
 }
